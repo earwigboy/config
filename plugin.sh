7dae382aca5e07ce6f732369c6a0eb5a2e681029
earwigboy <earwigboy@gmail.com>
2017-11-26 20:16:13 +0000

    Adding vim plugin download script

---
+++ plugin.sh
@@ -0,0 +1,39 @@
+#cd ~/.vim/bundle
+# YouCompleteMe		
+git clone https://github.com/valloric/youcompleteme
+#nerdtree		
+git clone https://github.com/scrooloose/nerdtree
+
+#closetag.vim		
+git clone https://github.com/docunext/closetag.vim
+
+
+#syntastic		
+git clone https://github.com/scrooloose/syntastic
+
+#vim-go
+git clone https://github.com/fatih/vim-go
+
+#delimitmate		
+git clone https://github.com/raimondi/delimitmate
+
+#typescript-vim		
+git clone https://github.com/leafgarland/typescript-vim
+
+#vim-sensible
+git clone https://github.com/tpope/vim-sensible
+
+#dracula-theme		
+git clone https://github.com/zenorocha/dracula-theme
+
+#vim-airline		
+git clone https://github.com/bling/vim-airline
+
+#vim-sleuth
+git clone https://github.com/tpope/vim-sleuth
+
+#emmet-vim		
+git clone https://github.com/mattn/emmet-vim
+
+#vim-surround
+git clone https://github.com/tpope/vim-surround
